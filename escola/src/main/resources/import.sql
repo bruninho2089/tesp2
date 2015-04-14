75dc023c84356c5d9a93c7bb8489bc0036a5588f
Bruno <Bruno@10.13.10.171>
2015-04-13 21:18:42 -0300

    Enviando exercicio 04

---
+++ escola/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
+INSERT INTO `tb_pessoa`(`id`, `cpf`, `nome`) VALUES (00001,12345678901,'Bruno Cesar')
+INSERT INTO `tb_pessoa`(`id`, `cpf`, `nome`) VALUES (00002,09876543212,'Cristiane Neiva')
+INSERT INTO `tb_pessoa`(`id`, `cpf`, `nome`) VALUES (00003,12345543211,'Leticia Cardoso')
+INSERT INTO `tb_aluno`(`dataAniversario`, `matricula`, `id`) VALUES ('1988-12-10',12345,00001)
+INSERT INTO `tb_aluno`(`dataAniversario`, `matricula`, `id`) VALUES ('1989-05-10',54321,00002)
+INSERT INTO `tb_professor`(`salario`, `id`) VALUES (4.500,00003)
